637c555a5600e4eca94cc7f245a220be1b259fb1
Milen Palachorov <feniks9511@gmail.com>
2024-02-04 13:43:57 +0200

    setup hbs

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-const express = require("express")
-const handlebars = require("express-handlebars")
-const path = require("path")
+const exp = require("constants");
+const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs',
-}))
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
 
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static("src/public"));
 
-app.get('/', (req, res) => {
-    res.render('home',)
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.listen(3000, () => {
-    console.log('Server is listnening on port 3000...')
-})
+  console.log("Server is listnening on port 3000...");
+});
