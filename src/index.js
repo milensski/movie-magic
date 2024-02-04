8c103cd9531098cc349b357f8939da196b4c88a7
Milen Palachorov <feniks9511@gmail.com>
2024-02-04 13:33:41 +0200

    setting view engine and adding static and html files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require("express")
+const handlebars = require("express-handlebars")
+const path = require("path")
 
 const app = express()
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}))
+
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+
+app.get('/', (req, res) => {
+    res.render('home',)
+})
 
 app.listen(3000, () => {
     console.log('Server is listnening on port 3000...')
